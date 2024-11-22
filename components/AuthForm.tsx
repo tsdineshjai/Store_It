0a573af11c6d148ce689072cb9e4c504f2cccb17
D JAI <tsdineshjai@gmail.com>
2024-11-22 13:35:18 +0800

    building authForm component

---
+++ components/AuthForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type AuthType = "sign-in" | "sign-up";
+
+const AuthForm = ({ type }: { type: AuthType }) => {
+	return <div>AuthForm</div>;
+};
+
+export default AuthForm;
