ad95717108443b2a5ff14b92af68d92d45eb2d96
D JAI <tsdineshjai@gmail.com>
2024-11-24 12:12:29 +0800

    authentication updation

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,6 +17,7 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { createAccount, signInUser } from "@/lib/actions/user.actions";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -33,6 +34,7 @@ const authFormSchema = (formType: FormType) => {
 const AuthForm = ({ type }: { type: FormType }) => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [errorMessage, setErrorMessage] = useState("");
+	const [accountId, setAccountId] = useState(null);
 
 	const formSchema = authFormSchema(type);
 	const form = useForm<z.infer<typeof formSchema>>({
@@ -45,9 +47,24 @@ const AuthForm = ({ type }: { type: FormType }) => {
 	});
 
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
-		// setIsLoading(true);
-		// setErrorMessage("");
-		console.log(values);
+		setIsLoading(true);
+		setErrorMessage("");
+
+		try {
+			const user =
+				type === "sign-up"
+					? await createAccount({
+							fullName: values.fullName || "",
+							email: values.email,
+						})
+					: await signInUser({ email: values.email });
+
+			setAccountId(user.accountId);
+		} catch {
+			setErrorMessage("Falied to create account. Please try again");
+		} finally {
+			setIsLoading(false);
+		}
 	};
 
 	return (
